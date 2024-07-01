6c249eaf17f888c47767b3ba3a0800934fc4af7a
BlazZ1t <klimspy@gmail.com>
2024-07-01 03:11:48 +0300

    Finished registration screen
    Took 31 minutes

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -8,10 +8,182 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String _selectedGender = 'Male';
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromRGBO(64, 107, 120, 1), Color.fromRGBO(29, 47, 56, 1)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DropdownButtonFormField(
+                  value: _selectedGender,
+                  decoration: const InputDecoration(
+                    labelText: "Gender",
+                    labelStyle: TextStyle(
+                      color: Colors.orange
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                      )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                      borderSide: BorderSide(
+                        color: Colors.orange
+                      )
+                    ),
+                  ),
+                  icon: null,
+                  dropdownColor: const Color.fromRGBO(29, 47, 56, 1),
+                  items: ['Male', 'Female', 'Battle Helicopter'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: const TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedGender = newValue!;
+                    });
+                  },
+                ),
+
+                const SizedBox(height: 16,),
+
+                TextField(
+                  controller: _ageController,
+                  decoration: const InputDecoration(
+                      labelText: 'Birthday',
+                      labelStyle: TextStyle(
+                          color: Colors.orange
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                          borderSide: BorderSide(
+                            color: Colors.orange,
+                          )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                          borderSide: BorderSide(
+                              color: Colors.orange
+                          )
+                      ),
+                      suffixIcon: Icon(Icons.edit)
+                  ),
+                  keyboardType: TextInputType.datetime,
+                  style: const TextStyle(
+                      color: Colors.white
+                  ),
+
+                ),
+
+                const SizedBox(height: 16),
+
+                TextField(
+                  controller: _heightController,
+                  decoration: const InputDecoration(
+                    labelText: 'Height',
+                    labelStyle: TextStyle(
+                        color: Colors.orange
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                        borderSide: BorderSide(
+                          color: Colors.orange,
+                        )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                        borderSide: BorderSide(
+                            color: Colors.orange
+                        )
+                    ),
+                    suffixIcon: Icon(Icons.edit)
+                  ),
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+
+                const SizedBox(height: 16),
+
+                TextField(
+                  controller: _weightController,
+                  decoration: const InputDecoration(
+                      labelText: 'Weight',
+                      labelStyle: TextStyle(
+                          color: Colors.orange
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                          borderSide: BorderSide(
+                            color: Colors.orange,
+                          )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                          borderSide: BorderSide(
+                              color: Colors.orange
+                          )
+                      ),
+                      suffixIcon: Icon(Icons.edit)
+                  ),
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+
+                const SizedBox(height: 70),
+
+                ElevatedButton(
+                    onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/home');
+                      },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(29, 47, 56, 1),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      )
+                    ),
+                    child: const Text(
+                      "Continue",
+                      style: TextStyle(
+                        color: Colors.orange
+                      ),
+                    )
+                )
+              ],
+            ),
+          ),
+        )
+      ),
+    );
   }
 
 }
